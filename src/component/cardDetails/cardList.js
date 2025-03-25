da3e4926464cd0fc158aa78e269ff17b902bdc3f
Pranay Nandiwadekar <142014316+pranayndv@users.noreply.github.com>
2025-03-25 13:19:12 +0530

    Update cardList.js

--- src/component/cardDetails/cardList.js
+++ src/component/cardDetails/cardList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FadeLoader } from "react-spinners";
-import MaskNumber from "../MaskNumber";
+import {MaskNumber} from "../MaskNumber";
 
 
 
